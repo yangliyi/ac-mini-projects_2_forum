792e709c33620923cc026e181bfb58e599d1329d
Yang Li-Yi <joseph.lyyang@gmail.com>
2015-09-30 22:37:16 +0800

    Add sort-by-category

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,12 +3,30 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-    if params[:order] == "topic"
-      @posts = Post.order('updated_at desc').page(params[:page]).per(5)
-    elsif params[:order] == "replies"
-      @posts = Post.order("comments_count desc").page(params[:page]).per(5)
+
+
+    @posts = Post.all
+
+    if params[:category_id]
+
+      @category = Category.find(params[:category_id])
+      @posts = @category.posts
+
+      if params[:order] == "topic"
+        @posts = @posts.order('updated_at desc').page(params[:page]).per(5)
+      elsif params[:order] == "replies"
+        @posts = @posts.order("comments_count desc").page(params[:page]).per(5)
+      else
+        @posts = @posts.order('last_comment desc').page(params[:page]).per(5)
+      end
     else
-      @posts = Post.order('last_comment desc').page(params[:page]).per(5)
+      if params[:order] == "topic"
+        @posts = @posts.order('updated_at desc').page(params[:page]).per(5)
+      elsif params[:order] == "replies"
+        @posts = @posts.order("comments_count desc").page(params[:page]).per(5)
+      else
+        @posts = @posts.order('last_comment desc').page(params[:page]).per(5)
+      end
     end
   end
 
