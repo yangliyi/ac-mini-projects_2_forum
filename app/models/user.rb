40cdadb2ff0b0a98d76cae3a927ff0c0c3465fa8
Yang Li-Yi <joseph.lyyang@gmail.com>
2015-10-02 02:28:23 +0800

    Add Gravatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  include Gravtastic
+  gravtastic :secure => true,
+              :filetype => :gif,
+              :size => 50
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
