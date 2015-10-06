41ea177dd8c46a3a8ddce84d83097d9b732817e4
Yang Li-Yi <joseph.lyyang@gmail.com>
2015-10-06 13:23:04 +0800

    Add admin or comment user checking to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,13 @@
 class CommentsController < ApplicationController
 
+  before_action :authenticate_user!
+  before_action :set_post
+
   def create
-    @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
     @comment.user = current_user
     @post.last_comment = Time.now
+
     if @comment.save
       @post.save
       flash[:notice] = "回覆成功！"
@@ -12,15 +15,21 @@ class CommentsController < ApplicationController
     end
   end
 
-
   def destroy
-    @post = Post.find(params[:post_id])
-    @comment = @post.comments.find(params[:id])
-    @comment.destroy
+    if current_user.admin? || current_user = @comment.user
+      @comment = @post.comments.find(params[:id])
+      @comment.destroy
+    else
+      notice: "抱歉你沒有刪除文章的權限"
+    end
     redirect_to post_path(@post)
   end
 
   private
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
 
   def comment_params
     params.require(:comment).permit(:commenter, :comment, :user_id)
